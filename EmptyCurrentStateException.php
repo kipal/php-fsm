684497a681edfe6b86037ecb061463353e4c5c7f
Nándor Kiss <kipal42@gmail.com>
2016-03-03 11:28:18 +0100

    Create EmptyCurrentStateException.php

---
+++ EmptyCurrentStateException.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Copyright 2016 Nándor Kiss. All rights reserved.
+ * Use of this source code is governed by a MIT-style
+ * license that can be found in the LICENSE file.
+ */
+class EmptyCurrentStateException extends Exception
+{
+	/**
+	 * @param $methodName string
+	 * @param $input      Input
+	 */
+	public static function getInstance($methodName, Input $input)
+	{
+		return new EmptyCurrentStateException(
+			"Method '$methodName' with '$input' parameter returned null as state."
+		);
+	}
+}
